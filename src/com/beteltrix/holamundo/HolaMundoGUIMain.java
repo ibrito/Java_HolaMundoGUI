3ae749a5b85789130beacb26a908f9e5d3adc0dd
Irving Brito <ibrito.sistemas@gmail.com>
2015-09-28 10:50:14 -0430

    Inicio del Proyecto

---
+++ src/com/beteltrix/holamundo/HolaMundoGUIMain.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.beteltrix.holamundo;
+
+/**
+ *
+ * @author Irving Brito <ibrito.sistemas@gmail.com>
+ */
+public class HolaMundoGUIMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PantallaJFrame pjf = new PantallaJFrame();
+        pjf.setVisible(true);
+    }
+    
+}
